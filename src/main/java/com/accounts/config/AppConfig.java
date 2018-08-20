3d133cf09235a926da73255b332bda7cd6d48906
pratyush <singhpratyush47@gmail.com>
2018-08-20 23:42:29 +0530

    completed example finally

--- src/main/java/com/accounts/config/AppConfig.java
+++ src/main/java/com/accounts/config/AppConfig.java
@@ -36,7 +36,7 @@ public class AppConfig {
     @Bean
     public JdbcTemplate jdbcTemplate(DataSource dataSource)
     {
-        JdbcTemplate jdbcTemplate=new JdbcTemplate();
+        JdbcTemplate jdbcTemplate= new JdbcTemplate(dataSource);
         jdbcTemplate.setResultsMapCaseInsensitive(true);
         return jdbcTemplate;
     }
